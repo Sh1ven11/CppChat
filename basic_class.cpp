29a12745f8a7c17925a35356e073726aca9b4ddb
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-08-08 18:17:40 +0530

    login

--- basic_class.cpp
+++ basic_class.cpp
@@ -20,6 +20,15 @@ class User {
             cout << "Invalid password. Please try again." << endl;
         }
     }
+    bool login(string pass) {
+        if(pass == password) {
+            cout << "Login successful!" << endl;
+            return true;
+        } else {
+            cout << "Login failed. Incorrect password." << endl;
+            return false;
+        }
+    };
 
 
     private:
@@ -37,6 +46,7 @@ class User {
 
 int main(){
     int input=0;
+    unordered_map<string, User> users; // Store users by username
     while(input!=3){
         cout<<"1.new User \n 2.login \n 3 exit "<<endl;
         cin>>input;
@@ -49,11 +59,17 @@ int main(){
             cin>>password;
             try{
                 User newUser(username, password);
+                users[username] = newUser; // Store the new user in the map
             cout<<"User created successfully!"<<endl;}
             catch(const invalid_argument& e){
                 cout<<"User NOT created successfully"<<endl;
             
             }
-        }
+        } else if(input==2){
+            string username, password;
+            cout<<"Enter username: ";
+            cin>>username;
+            cout<<"Enter password: ";
+            cin>>password;
     }
 }
